49a9755330a54bf204add6f84e278723f3b85fb8
rubinarubi <36584734+rubinarubi@users.noreply.github.com>
2018-11-24 16:37:11 +0600

    Add files via upload

---
+++ a.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact us</title>
+	</br></br></br></br>
+	<center>
+	<address>
+	<h3>***** Stationary Shop *****<h3/>
+	<h4> Contact: 0175460289</h4>
+	<h4> Email: StationaryShop@yahoo.com</h4>
+
+	</address>
+	</center>
+</body>
+</html>
